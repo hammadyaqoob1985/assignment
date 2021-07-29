567e4a327f252f487feb5b8e8cca6d98d4039667
Hammad Yaqoob <hammad.yaqoob1985@yahoo.co.uk>
2021-07-29 22:43:50 +0100

    added stock controller with controller advice

--- src/main/java/com/simple/stock/market/assignment/calculator/PERatioCalculator.java
+++ src/main/java/com/simple/stock/market/assignment/calculator/PERatioCalculator.java
@@ -3,13 +3,16 @@ package com.simple.stock.market.assignment.calculator;
 import com.simple.stock.market.assignment.exception.InvalidDividendException;
 import com.simple.stock.market.assignment.model.Stock;
 import com.simple.stock.market.assignment.model.StockSymbol;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PERatioCalculator {
     private final List<Stock> stocks;
 
-    public PERatioCalculator(List<Stock> stocks) {
+    public PERatioCalculator(@Qualifier("stocks") List<Stock> stocks) {
         this.stocks = stocks;
     }
 
--- src/main/java/com/simple/stock/market/assignment/controller/StockController.java
+++ src/main/java/com/simple/stock/market/assignment/controller/StockController.java
@@ -1,6 +1,7 @@
 package com.simple.stock.market.assignment.controller;
 
 import com.simple.stock.market.assignment.calculator.DividendYieldCalculator;
+import com.simple.stock.market.assignment.calculator.PERatioCalculator;
 import com.simple.stock.market.assignment.model.StockSymbol;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,14 +9,22 @@ import org.springframework.web.bind.annotation.*;
 public class StockController {
 
     private final DividendYieldCalculator dividendYieldCalculator;
+    private final PERatioCalculator peRatioCalculator;
 
-    public StockController(DividendYieldCalculator dividendYieldCalculator) {
+    public StockController(DividendYieldCalculator dividendYieldCalculator, PERatioCalculator peRatioCalculator) {
         this.dividendYieldCalculator = dividendYieldCalculator;
+        this.peRatioCalculator = peRatioCalculator;
     }
 
     @RequestMapping(value = "/calculateDividendYield", method = RequestMethod.POST)
     @ResponseBody
-    public double calculate(@RequestParam StockSymbol stockSymbol, @RequestParam double price) {
+    public double calculateDividendYield(@RequestParam StockSymbol stockSymbol, @RequestParam double price) {
         return dividendYieldCalculator.calculateDividendYield(stockSymbol, price);
+    }
+
+    @RequestMapping(value = "/calculatePERatio", method = RequestMethod.POST)
+    @ResponseBody
+    public double calculatePERatio(@RequestParam StockSymbol stockSymbol, @RequestParam double price) {
+        return peRatioCalculator.calculatePERatio(stockSymbol, price);
     }
 }
